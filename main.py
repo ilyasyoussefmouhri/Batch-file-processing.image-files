7fb91e80ca85aeb2e5787ae5578ea43b73bb9c80
ilyasyoussefmouhri <144175695+ilyasyoussefmouhri@users.noreply.github.com>
2026-01-13 03:31:13 +0100

    Fix indentation in main function

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from processor import batch_process_files
 
 def main():
- # Setup argument parser
+    # Setup argument parser
     parser = argparse.ArgumentParser(description='Batch process images: rename, convert, and organize')
     parser.add_argument('folder_path', type=str, help='Path to the folder containing files to process')
     
